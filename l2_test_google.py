7f0fe60e90f674abb6c9a33ed79761f50d9af913
Nikita <nikitazhuravlevqa@gmail.cm>
2024-03-10 23:30:31 +0300

    Add fixture(browser_settings), negative test

--- l2_test_google.py
+++ l2_test_google.py
@@ -1,6 +1,16 @@
+import pytest
 from selene import browser, have
+@pytest.fixture
+def browser_settings():
+    browser.config.window_width = 1000
+    browser.config.window_height = 1000
 
-browser.open("https://www.google.com/")
-def test_google():
+def test_google_positive(browser_settings):
+    browser.open("https://www.google.com/")
     browser.element("#APjFqb").type("yashaka/selene").press_enter()
     browser.element("[id=search]").should(have.text("yashaka/selene: User-oriented Web UI browser"))
+
+def test_google_negative(browser_settings):
+    browser.open("https://www.google.com/")
+    browser.element("#APjFqb").type("1165212496249519751759157").press_enter().press_enter()
+    browser.element(".card-section").should(have.text("По запросу 1165212496249519751759157 ничего не найдено. "))
